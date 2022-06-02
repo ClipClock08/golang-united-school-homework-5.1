1b3347ca649024187df0e46860c6bec96261afc5
“Oleksandr <oleksandr_lopatin@epam.com>
2022-06-02 12:26:15 +0300

    after testing

--- square.go
+++ square.go
@@ -4,14 +4,14 @@ type Point struct {
 	x, y int
 }
 type Square struct {
-	P Point
-	a uint
+	start Point
+	a     uint
 }
 
-func (s *Square) endPoint() Point {
+func (s *Square) End() Point {
 	return Point{
-		x: s.P.x + int(s.a),
-		y: s.P.y + int(s.a),
+		x: s.start.x + int(s.a),
+		y: s.start.y + int(s.a),
 	}
 }
 func (s *Square) Area() uint {
